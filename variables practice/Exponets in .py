723734cf2d0cd093c7f6ac91e3685b0d4ee03d08
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-12 03:41:59 +0000

    made print statements and made variables in python

---
+++ variables practice/Exponets in .py
@@ -0,0 +1,26 @@
+a = 1
+b = 2
+c = 3
+d = 4 
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 18
+k = 12
+l = 72
+m = 17
+n = 24
+o = 15
+p = 22
+q = 2.5
+print(int(g-n/h*d+f))
+print(int(j/c-g+b*e))
+print(int(f*d/k+l/h-i))
+print(int((m-f/b)+d*c))
+print(int(-b(a*d-b/b)+(f+b-c)))
+print(int(-a*[(c-d*g)/e]-b*n/6))
+print(int(c*e*e/o)-(e-b*b))
+print(int(a*a*a*a*b*b+c*c*c)-b*b*b*b*b/e)
+print(int(p/b-q)**+(d-f/f)*(d-f/f))
