77c244983f07cb2b228f17df3a78113f1315a1ec
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-18 23:49:57 +0000

    changed stuff in C

---
+++ More advanced programming/.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void decorate_name(const char* name) {
+    printf("<<< %s >>>\n", name);
+}
+
+int main() {
+    char name[100];
+     printf("Please enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    size_t len = strlen(name);
+    if (len > 0 && name[len - 1] == '\n') {
+        name[len - 1] = '\0';
+    return 0;
+}
