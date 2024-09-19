c33f25903ede55ad57ee1a85806e3c7667b7e1e2
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-19 20:41:53 +0000

    sigma

---
+++ stings/sillysentences.py
@@ -0,0 +1,6 @@
+print("this will make a funny story")
+name = input("type a name: ")
+place = input("type a place: ")
+verb = input("type a verb: ")
+noun = input("type a noun: ")
+print(name, " went to "place, "where they" )
