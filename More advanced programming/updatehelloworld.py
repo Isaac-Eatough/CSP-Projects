57d9138d4be2d5abd9dcef393eca0fbb29350954
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-23 20:41:37 +0000

    r

---
+++ More advanced programming/updatehelloworld.py
@@ -0,0 +1,9 @@
+name = input("type a name")
+def nameFuntion(Hello):
+    print("Hello", name)
+    
+    nameFunction(input("type a name"))
+    nameFunction(input("type a name"))
+    nameFunction(input("type a name"))
+    nameFunction(input("type a name"))
+    nameFunction(input("type a name"))
