f697c2dcb9b2401d53745304cbe0a30d526170db
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-25 20:41:36 +0000

    sigma

--- More advanced programming/updatehelloworld.c
+++ More advanced programming/updatehelloworld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+#include <string.h>
+char name;
+char colt[4];
+char steve[5];
+char larry[5];
+char leon[4];
+char doug[4];
 int main(void){
     char name[20];
     return 0;
