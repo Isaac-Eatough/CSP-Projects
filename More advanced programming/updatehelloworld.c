24db2dba4b887b3d19d3a4bcd74d9bf51709d303
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-26 20:41:23 +0000

    s

--- More advanced programming/updatehelloworld.c
+++ More advanced programming/updatehelloworld.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-float doug, sam, hank, larry, leon;
+char input(char type[], char var){
+    printf("type a %s: \n", type);
+    scanf("%s", &var);
+    return var;
+}
 
 int main(void){
-    printf("this will chose 5 different names");
-    doug = input("doug", doug);
-    sam = input("sam", sam);
-    hank = input("hank", hank);
-    larry = input("larry", larry);
-    leon = input("leon", leon);
+input("name: \n")
     return 0;
 }
