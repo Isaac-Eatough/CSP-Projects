26886beee4d43e6bf7594c5e976ff347136e424a
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-09-27 20:37:01 +0000

    done did it

--- More advanced programming/updatehelloworld.c
+++ More advanced programming/updatehelloworld.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-char input(char type[], char var){
-    printf("type a %s: \n", type);
-    scanf("%s", &var);
-    return var;
+char input(char type[]){
+    printf("hello %s \n", type);
 }
 
 int main(void){
-input("name: \n")
+    char name1[10] = "Frank";
+   input(name1);
+   char name2[10] = "Doug";
+   input(name2);
+   char name3[10] = "Larry";
+   input(name3);
+   char name4[10] = "Chester";
+   input(name4);
+   char name5[10] = "Gus";
+   input(name5);
     return 0;
 }
