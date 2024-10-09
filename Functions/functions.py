61e920297494a8353214cc95d3fdce7a9ff6aab0
Isaac-Eatough <isaac.eatough@ucas-edu.net>
2024-10-09 20:41:35 +0000

    became sigma

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,4 +1,5 @@
-
+def float(float, type)
+    print(f"what is your {type}: ")
 print("Hello and welcome to your finacial calculator!\n")
 income =float(input("what is your monthly income: "))
 rent =float(input("what is your monthly rent: "))
@@ -8,12 +9,6 @@ transportation =float(input("what is your monthly transportation cost: "))
 savings = income*.2
 expenses = rent+utilties+groceries+transportation
 spending = income-savings-expenses
-prent = income/rent *100
-putilties = income/utilties *100
-pgroceries = income/groceries *100
-ptransportation = income/transportation *100
-psavings = income/savings *100
-pexpenses = income/expenses *100
 def percent(type, amount):
     per = amount/income
     print(f"your {type} is {per}% income.")
@@ -21,9 +16,9 @@ print(f"your monthly income is ${income:.2f}\n")
 print(f"your monthly expenses is ${expenses:.2f}\n")
 print(f"your monthly savings is ${savings:.2f}\n")
 print(f"your monthly spending money is ${spending:.2f}\n")
-percent("rent",rent)
-percent("utilities",utilties)
-percent("rent",rent)
-percent("rent",rent)
-percent("rent",rent)
-percent("rent",rent)
+print(percent("rent",rent))
+print(percent("utilities",utilties))
+print(percent("groceries",groceries))
+print(percent("transportation",transportation))
+print(percent("savings",savings))
+print(percent("expenses",expenses))
